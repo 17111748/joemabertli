24f2c6ecc8b1c13288e3a666b28bb52c9e561ef0
Anna Li <ayli1@ece003.ece.local.cmu.edu>
2021-10-29 02:03:41 -0400

    Fixing compilation errors

--- Sim/tcf_dump.tcl
+++ Sim/tcf_dump.tcl
@@ -1,5 +1,5 @@
 
 # change DUT name and output file name/path
-dumptcf -scope design.DUT -internal -memories -overwrite -output "tcf_gen/design.tcf" -inctoggle
+dumptcf -scope design.dut -internal -memories -overwrite -output "tcf_gen/design.tcf" -inctoggle
 run
 exit
--- Sim/vcd_dump.tcl
+++ Sim/vcd_dump.tcl
@@ -1,6 +1,6 @@
 # file path to dump vcd
-database -open -vcd dump -into ./vcd_gen/design.vcd
-# change design.DUT name to top level name  
-probe -create design.DUT -depth to_cells  -vcd -all -database dump -all -memories
+database -open -vcd dump -into ./unary_binary.vcd
+# change design.DUT name to top level name
+probe -create unary_binary_mxu_tb.dut -depth to_cells  -vcd -all -database dump -all -memories
 run
 exit
