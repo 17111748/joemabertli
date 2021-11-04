f7f4fa8384a0b68242fc98d9c70794d123bc4069
Anna Li <ayli1@andrew.cmu.edu>
2021-11-04 17:14:38 -0400

    Removed old files from parent branch

--- Sim/xcelium.d/run.lnx86.19.03.d/.timestamp.ts
+++
@@ -1,3 +0,0 @@
-1635480850 /afs/ece.cmu.edu/usr/ayli1/Private/joemabertli/Src/unary_binary_mac.sv
-1635487384 /afs/ece.cmu.edu/usr/ayli1/Private/joemabertli/Src/unary_binary_mxu_tb.sv
-1635487782 /afs/ece.cmu.edu/usr/ayli1/Private/joemabertli/Src/unary_binary_mxu.sv
