ef1cd5428bf306ec5250af3c33c51b8bb2579169
Anna Li <ayli1@ece003.ece.local.cmu.edu>
2021-11-19 01:27:46 -0500

    Got alpha working

--- Sim/vcd_dump.tcl
+++ Sim/vcd_dump.tcl
@@ -1,6 +1,6 @@
 # file path to dump vcd
-database -open -vcd dump -into ./unary_binary.vcd
+database -open -vcd dump -into ./unary_multiplier.vcd
 # change design.DUT name to top level name
-probe -create unary_binary_mxu_tb.dut -depth to_cells  -vcd -all -database dump -all -memories
+probe -create unary_multiplier_tb.dut -depth to_cells  -vcd -all -database dump -all -memories
 run
 exit
---
+++ Sim/xcelium.d/run.lnx86.19.03.d/files.ts
@@ -0,0 +1,2 @@
+1637303148 /afs/ece.cmu.edu/usr/ayli1/Private/joemabertli/Src/unary_multiplier_cycle_reduce.sv
+1637303237 /afs/ece.cmu.edu/usr/ayli1/Private/joemabertli/Src/unary_multiplier_tb.sv
